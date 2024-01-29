d4108f1cbb583f026d4a5722d8a9dfd494df7a10
bio-ai-source <cctvcctv700@163.com>
2024-01-29 13:18:45 +0800

    base network for MUMA

--- net.py
+++ net.py
@@ -7,8 +7,6 @@ import torch.nn.init as init
 
 
 def to_var(x, requires_grad=True):
-    # if torch.cuda.is_available():
-    #     x = x.cuda()
     return Variable(x, requires_grad=requires_grad)
 
 
